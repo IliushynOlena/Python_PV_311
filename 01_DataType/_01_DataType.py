5b9d8bb9f0607aa72fc48a09ca9184039e191925
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2023-12-08 20:25:46 +0200

    Add project files.

---
+++ 01_DataType/_01_DataType.py
@@ -0,0 +1,161 @@
+# print("Python")
+# print("P","y","t","h","o","n",sep="_ _", end=" ")
+# print("Hello world")
+
+# print(123)
+# print('c')
+# print('123')
+
+#Data types
+#numbers --> int , float
+#str
+#boolean - True False
+#Ctrl+D
+# print("int", 2)
+# print("float", 3.14)
+# print("str", "5.6")
+#Ctrl+K+C
+# print("int",type(2))
+# print("float", type( 3.14))
+# print("str", type("5.6"))
+# print("bool", type(True))
+# print("bool", type(False))
+# # +, - , * , /, %, **, //
+# print(2+2)
+# print(2**2)
+# print(2**3.0)
+# print(2.0**3.0)
+
+# print(2*3)
+# print(2*3.0)
+# print(2.0*3)
+
+# print(9/3)
+# print(9/3.0)
+# print(9.0/6)
+
+# print(9//3)
+# print(9//3.0)
+# print(9.0//6)
+
+
+# print(15%5)
+# print(9%4)
+
+# #print(5/0)
+# #print(5%0)
+
+# print(-8 + 6)
+# print(9-4)
+# print(8*9)
+# print(18/6)
+
+# # - +
+# print(-5)
+# print(+1000)
+
+# print(2+3*4)
+
+# print(2 ** 2 ** 3)#64   512   256  
+#2**3 = 8
+#2**8 = 256
+
+# print(2 * 2 * 3)#=>>>>
+
+# print(5 + 3.6)
+# #print(5 + "friend")
+# print("5" + "friend")
+# #True = 1   False = 0
+# print(5 + True)#
+# print(5 + False)
+
+# print(100_000_000_000)
+
+# print(1e11)
+# print(10e10)
+
+# age_of_human = 15
+# print("age_of_human")
+# print(age_of_human)
+# AgeOfHuman = 55
+# print(AgeOfHuman)
+# _number = 45
+# print(_number)
+# #5num = 78
+# Number = 100
+# number = 11
+
+# var = 1#initialization - int
+# print(var)
+
+# var = 100
+# print(var)
+
+# account_balance = 1000.0#float
+# client_name = "Bob"#str
+
+# print(var, account_balance, client_name)
+
+# Var = "3.7.1"
+# print("Python version " + Var)
+
+# from email import message
+
+
+# a = 3.0
+# b = 4.7
+# c = (a ** 2 + b ** 2)**0.5
+# print("c = ", c)
+
+# first_number = 10
+# second_number = 22
+# result = first_number + second_number
+# print("Result = ", result)
+
+# first_number = first_number + 15
+# print(first_number)
+
+# first_number+= 15
+# print(first_number)
+
+# first_number*= 15
+# print(first_number)
+
+# first_number*= 15
+# print(first_number)
+
+# first_number/= 15
+# print(first_number)
+
+# line_1 = "Hyper"#string
+# line_2 = "Text"#string
+# line_3 = "Markup"#string
+# line_4 = "Language"#string
+
+# message = line_1+ " " + line_2+" " + line_3+" " + line_4#string
+# print("="*30)
+# print(message*3)
+# print("======================================")
+
+# #print("\033[31m",line_1)
+
+# #print("\033[31mHello  {} {} {} ".format(line_1, line_2, line_3))
+# print("\033[31mline_1")
+
+# print("\033[31m\033[47m\033[1m{}\033[0m".format(line_1), end=" ")
+# print("\033[32m\033[46m\033[2m{}\033[0m".format(line_2),end=" ")
+# print("\033[33m\033[45m\033[3m{}\033[0m".format(line_3),end=" ")
+# print("\033[34m\033[44m\033[4m{}\033[0m".format(line_4),end=" ")
+
+
+print("Tell me anything....")
+anything  = input()
+print(anything)
+
+a= int(input("Enter number a : "))
+b = int( input("Enter number b : "))
+res = a + b
+print("Res = ", res)
+
+
+
